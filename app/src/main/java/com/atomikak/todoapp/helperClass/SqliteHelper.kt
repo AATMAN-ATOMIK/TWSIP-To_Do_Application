e03eaa8a123aaa48fc9558b33415a0379b7e1a08
Aatman <aatmankacha@gmail.com>
2023-08-09 00:09:24 +0530

    Added Notification, Update,sorting and testing pending

--- app/src/main/java/com/atomikak/todoapp/helperClass/SqliteHelper.kt
+++ app/src/main/java/com/atomikak/todoapp/helperClass/SqliteHelper.kt
@@ -5,16 +5,21 @@ import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
 import android.widget.Toast
 import java.lang.Exception
 
 class SqliteHelper(val context: Context) : SQLiteOpenHelper(context, "MyTasks", null, 1) {
     override fun onCreate(db: SQLiteDatabase?) {
         try {
-            db!!.execSQL("CREATE TABLE IF NOT EXISTS Category ( c_id INTEGER PRIMARY KEY AUTOINCREMENT , c_name TEXT )")
-            db.execSQL("CREATE TABLE IF NOT EXISTS Tasks ( t_id INTEGER PRIMARY KEY AUTOINCREMENT ,c_id INTEGER ,  t_title TEXT , t_des TEXT , t_time TEXT , t_date Date , t_status TEXT , FOREIGN KEY (c_id) REFERENCES Category (c_id) )")
+            db!!.execSQL("CREATE TABLE Category ( c_id INTEGER PRIMARY KEY AUTOINCREMENT , c_name TEXT )")
+            db!!.execSQL("CREATE TABLE Tasks ( t_id INTEGER PRIMARY KEY AUTOINCREMENT ,c_name TEXT ,t_title TEXT ,t_des TEXT ,t_time TEXT ,t_date Date ,t_status TEXT,t_priority TEXT)")
+//            val cv = ContentValues()
+//            cv.put("c_name", "Daily")
+//            db.insert("Category", null, cv)
+
         } catch (e: Exception) {
-            Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
+            Log.d("ERR: ",e.message.toString())
         }
     }
 
@@ -36,11 +41,10 @@ class SqliteHelper(val context: Context) : SQLiteOpenHelper(context, "MyTasks", 
         return res.toInt() != -1
     }
 
-    fun showCategory():Cursor{
+    fun showCategory(): Cursor {
         val db = this.readableDatabase
-        return db.rawQuery("SELECT * FROM Category",null)
+        return db.rawQuery("SELECT * FROM Category", null)
     }
-
 
 
     //Task Functions
@@ -48,11 +52,13 @@ class SqliteHelper(val context: Context) : SQLiteOpenHelper(context, "MyTasks", 
     fun addTask(task: Task): Boolean {
         val db = this.writableDatabase
         var contentValue: ContentValues = ContentValues()
-        contentValue.put("c_id", task.c_id.toString())
+        contentValue.put("c_name", task.c_name.toString())
         contentValue.put("t_title", task.t_title.toString())
         contentValue.put("t_des", task.t_des.toString())
         contentValue.put("t_time", task.t_time.toString())
         contentValue.put("t_date", task.t_date.toString())
+        contentValue.put("t_status", task.t_status.toString())
+        contentValue.put("t_priority", task.t_priority.toString())
         val res = db.insert("Tasks", null, contentValue)
         return res.toInt() != -1
     }
@@ -61,12 +67,13 @@ class SqliteHelper(val context: Context) : SQLiteOpenHelper(context, "MyTasks", 
     fun updateTask(task: Task, t_id: Int): Boolean {
         val db = this.writableDatabase
         var contentValue: ContentValues = ContentValues()
-        contentValue.put("c_id", task.c_id.toString())
+        contentValue.put("c_name", task.c_name.toString())
         contentValue.put("t_title", task.t_title.toString())
         contentValue.put("t_des", task.t_des.toString())
         contentValue.put("t_time", task.t_time.toString())
         contentValue.put("t_date", task.t_date.toString())
         contentValue.put("t_status", task.t_status.toString())
+        contentValue.put("t_priority", task.t_priority.toString())
         val res = db.update("Tasks", contentValue, "t_id = ", arrayOf(t_id.toString()))
         return res != -1
     }
@@ -85,18 +92,20 @@ class SqliteHelper(val context: Context) : SQLiteOpenHelper(context, "MyTasks", 
         return res != -1
     }
 
-    fun showTask(sort: String? = null, status: String? = null, sortStyle: String): Cursor {
+    fun showTask(sort: String? = null, status: String? = null, sortStyle: String? = null): Cursor {
         val db = this.readableDatabase
-        return if (sort == null && status == null) {
+        return if (sort == null && status == null && sortStyle == null) {
             db.rawQuery("SELECT * FROM Tasks", null)
         } else {
             if (sort == null) {
                 db.rawQuery("SELECT * FROM Tasks WHERE t_status = ?", arrayOf(status))
             } else if (status == null) {
                 db.rawQuery("SELECT * FROM Tasks ORDER BY $sort $sortStyle", null)
-            }else{
-                db.rawQuery("SELECT * FROM Tasks ORDER BY $sort $sortStyle WHERE t_status = ?",
-                    arrayOf(status))
+            } else {
+                db.rawQuery(
+                    "SELECT * FROM Tasks ORDER BY $sort $sortStyle WHERE t_status = ?",
+                    arrayOf(status)
+                )
             }
         }
     }
--- app/src/main/java/com/atomikak/todoapp/my_activities/MainActivity.kt
+++ app/src/main/java/com/atomikak/todoapp/my_activities/MainActivity.kt
@@ -1,29 +1,46 @@
 package com.atomikak.todoapp.my_activities
 
+import android.Manifest
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.atomikak.todoapp.R
 import com.atomikak.todoapp.adapters.CategoryAdapter
+import com.atomikak.todoapp.adapters.TaskAdapter
 import com.atomikak.todoapp.helperClass.Category
 import com.atomikak.todoapp.helperClass.SqliteHelper
+import com.atomikak.todoapp.helperClass.Task
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.security.Permission
 
 class MainActivity : AppCompatActivity() {
 
+    //widgets
     private lateinit var m_recv_category: RecyclerView
     private lateinit var m_recv_my_task: RecyclerView
-    private lateinit var categoryAdapter: CategoryAdapter
-
-    //    private lateinit var m_recv_my_task: TaskAdapter
     private lateinit var sortByDate: TextView
     private lateinit var m_addTask: FloatingActionButton
-    private lateinit var categoryList: ArrayList<String>
 
-    //    private lateinit var taskList: ArrayList<Task>
+
+    //adapters
+    private lateinit var categoryAdapter: CategoryAdapter
+    private lateinit var taskAdapter: TaskAdapter
+
+    //Helper
+    private lateinit var categoryList: ArrayList<String>
+    private lateinit var taskList: ArrayList<Task>
     private lateinit var sqliteHelper: SqliteHelper
 
 
@@ -35,6 +52,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun init() {
+
+        getPermissions()
+
+
         m_recv_category = findViewById(R.id.m_recv_category)
         m_recv_my_task = findViewById(R.id.m_recv_my_task)
         sortByDate = findViewById(R.id.sortByDate)
@@ -44,18 +65,127 @@ class MainActivity : AppCompatActivity() {
 
         //Category Recyclerview initialization
         categoryList = Category(sqliteHelper).getCategory()
-        categoryAdapter = CategoryAdapter(this@MainActivity,categoryList)
-        m_recv_category.layoutManager = LinearLayoutManager(this@MainActivity,LinearLayoutManager.HORIZONTAL,false)
+        categoryAdapter = CategoryAdapter(this@MainActivity, categoryList)
+        m_recv_category.layoutManager =
+            LinearLayoutManager(this@MainActivity, LinearLayoutManager.HORIZONTAL, false)
         m_recv_category.setHasFixedSize(true)
         m_recv_category.adapter = categoryAdapter
+
+        taskList = ArrayList()
+        getTaskList()
+        taskAdapter = TaskAdapter(this@MainActivity, taskList)
+        m_recv_my_task.layoutManager =
+            LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
+        m_recv_my_task.setHasFixedSize(true)
+        m_recv_my_task.adapter = taskAdapter
+
+
 
 
 
         m_addTask.setOnClickListener {
             val intent = Intent(this@MainActivity, AddTaskActivity::class.java)
             startActivity(intent)
+            this.finish()
         }
     }
+
+    private fun getPermissions() {
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this@MainActivity,
+                    Manifest.permission.POST_NOTIFICATIONS
+                )
+            ) {
+                val alertDialog = AlertDialog.Builder(this@MainActivity)
+                    .setTitle("Permission Required")
+                    .setMessage("Notification Permission is required in order to get notification of your tasks.")
+                    .setPositiveButton("Allow", object : DialogInterface.OnClickListener {
+                        override fun onClick(dialog: DialogInterface?, which: Int) {
+                            ActivityCompat.requestPermissions(
+                                this@MainActivity,
+                                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                                200
+                            )
+                        }
+                    })
+                    .setNegativeButton("Deny", object : DialogInterface.OnClickListener {
+                        override fun onClick(dialog: DialogInterface?, which: Int) {
+                            dialog!!.dismiss()
+                        }
+                    })
+                    .create()
+
+                alertDialog.show()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this@MainActivity,
+                    arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                    200
+                )
+            }
+        }
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.USE_EXACT_ALARM
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this@MainActivity,
+                    Manifest.permission.USE_EXACT_ALARM
+                )
+            ) {
+                val alertDialog = AlertDialog.Builder(this@MainActivity)
+                    .setTitle("Permission Required")
+                    .setMessage("Notification Permission is required in order to get notification of your tasks.")
+                    .setPositiveButton("Allow", object : DialogInterface.OnClickListener {
+                        override fun onClick(dialog: DialogInterface?, which: Int) {
+                            ActivityCompat.requestPermissions(
+                                this@MainActivity,
+                                arrayOf(Manifest.permission.USE_EXACT_ALARM),
+                                200
+                            )
+                        }
+                    })
+                    .setNegativeButton("Deny", object : DialogInterface.OnClickListener {
+                        override fun onClick(dialog: DialogInterface?, which: Int) {
+                            dialog!!.dismiss()
+                        }
+                    })
+                    .create()
+
+                alertDialog.show()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this@MainActivity,
+                    arrayOf(Manifest.permission.USE_EXACT_ALARM),
+                    200
+                )
+            }
+        }
+    }
+        private fun getTaskList() {
+            val cr = sqliteHelper.showTask()
+            while (cr.moveToNext()) {
+                taskList.add(
+                    Task(
+                        c_name = cr.getString(1).toString(),
+                        t_title = cr.getString(2).toString(),
+                        t_des = cr.getString(3).toString(),
+                        t_time = cr.getString(4).toString(),
+                        t_date = cr.getString(5).toString(),
+                        t_status = cr.getString(6).toString(),
+                        t_priority = cr.getString(7).toString()
+                    )
+                )
+            }
+        }
 
     override fun onBackPressed() {
         super.onBackPressed()
